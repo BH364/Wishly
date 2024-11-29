b30c36eee1450d6d56e75dc6995ce591c7ab768b
BH364 <balagaharika9@gmail.com>
2024-11-29 18:55:10 +0530

    best seller

--- Frontend/src/components/BestSeller.jsx
+++ Frontend/src/components/BestSeller.jsx
@@ -7,8 +7,10 @@ const BestSeller = () => {
     const {products} =useContext(ShopContext);
     const [bestSeller,setBestSeller]= useState([]);
     useEffect(()=>{
+        console.log(products);
        const bestProduct= products.filter((item)=>(item.bestseller));
-       setBestSeller(bestProduct.slice(0,5));
+       const shuffledProducts = bestProduct.sort(() => Math.random() - 0.5);
+       setBestSeller(shuffledProducts.slice(0,5));
     },[products]) 
   return (
     <div className='my-10'>
@@ -20,7 +22,7 @@ const BestSeller = () => {
         <div className='grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
             {
                 bestSeller.map((item,index)=>(
-                    <ProductItem key={index} id={item._id} name={item.name} image={item.image} price={item.price} />
+                    <ProductItem key={index} id={item._id} name={item.name} image={item.image} price={item.price} bestseller={item.bestseller}/>
                 ))
             }
         </div>
--- Frontend/src/components/ProductItem.jsx
+++ Frontend/src/components/ProductItem.jsx
@@ -2,17 +2,26 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { Link } from 'react-router-dom';
 
-const ProductItem = ({id,image,name,price}) => {
-    const {currency}= useContext(ShopContext);
-    return (
-      <Link className='text-gray-700 cursor-pointer' to={`/products/${id}`}>
-        <div className='overflow-hidden'>
-            <img src={image[0]} alt="image" className='hover:scale-110 transition ease-in-out'/>
-        </div>
-        <p className='pt-3 pb-1 text-sm'>{name}</p>
-        <p className='text-sm font-medium'>{currency}{price}</p>
-      </Link>
-  )
+const ProductItem = ({ id, image, name, price, bestseller }) => {
+  const { currency } = useContext(ShopContext);
+
+  return (
+    <Link className='text-gray-700 cursor-pointer relative' to={`/products/${id}`}>
+      <div className='overflow-hidden'>
+        <img src={image[0]} alt={name} className='w-full h-auto hover:scale-110 transition ease-in-out' />
+        
+        
+        {bestseller && (
+          <div className='absolute top-1 left-2 bg-cyan-500 text-white text-xs font-semibold px-2 py-1 rounded-md z-10'>
+            Best Seller
+          </div>
+        )}
+      </div>
+
+      <p className='pt-3 pb-1 text-sm'>{name}</p>
+      <p className='text-sm font-medium'>{currency}{price}</p>
+    </Link>
+  );
 }
 
-export default ProductItem
+export default ProductItem;
