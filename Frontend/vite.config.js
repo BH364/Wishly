37b1a1b4324bf547fac93f253324eef256769b04
BH364 <balagaharika9@gmail.com>
2024-11-29 23:47:05 +0530

    first

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ensures Vercel serves the app correctly from the root
-  build: {
-    outDir: 'dist', // This is the output directory where Vercel expects the build files
-  },
 })
