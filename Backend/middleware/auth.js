a282037e7996e17b327fee67ff024628671bc2dd
BH364 <balagaharika9@gmail.com>
2024-12-05 14:19:51 +0530

    token

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 
 
 const authUser = async (req,res,next)=>{
-    const token = req.cookies.token || req.headers['authorization']?.split(' ')[1];
+    const token = req.cookies.token || req.headers.token;
 
     if(!token ){
         return res.json({success:false,message:'Not Authorized Login Again'})
